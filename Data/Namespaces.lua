9f88091cfc83434e77c17b0e37ccd7fc638ec103
EdvardXYZ <eddumansky@gmail.com>
2021-08-10 17:44:26 +0300

    Initial commit

---
+++ Data/Namespaces.lua
@@ -0,0 +1,7 @@
+-- Used to create tables for folders in the file structure as they
+-- are part of the program hierarchy but don't have a dedicated file.
+TheEye = TheEye or {}
+
+TheEye.Recorder =
+{
+}
