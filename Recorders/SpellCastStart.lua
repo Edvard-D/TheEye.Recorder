ba548fc6f5b5679f9eef41511dc93418595d66c0
EdvardXYZ <eddumansky@gmail.com>
2021-08-11 20:39:52 +0300

    Implement SpellCastStart recorder

--- Recorders/SpellCastStart.lua
+++ Recorders/SpellCastStart.lua
@@ -1,2 +1,25 @@
 TheEye.Recorder.Recorders.SpellCastStart = {}
 local this = TheEye.Recorder.Recorders.SpellCastStart
+
+local DataRecord = TheEye.Recorder.Managers.Recorders.DataRecord
+local EventsRegister = TheEye.Core.Managers.Events.Register
+local UnitGUID = UnitGUID
+
+
+function this.Initialize()
+    this.gameEvents = { "UNIT_SPELLCAST_START" }
+    EventsRegister(this)
+end
+
+function this:OnEvent(event, ...)
+    unit, _, spellID = ...
+
+    if unit == "player" then
+        local data =
+        {
+            unitGUID = UnitGUID(unit),
+            spellID = spellID,
+        }
+        DataRecord("SpellCastStart", data)
+    end
+end
