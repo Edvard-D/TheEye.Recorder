3cdd1de915c536dc687316905f22d082502cae90
EdvardXYZ <eddumansky@gmail.com>
2021-08-12 16:42:16 +0300

    Implement PLAYER_PVP_TALENT recorder

--- Recorders/PLAYER_PVP_TALENT.lua
+++ Recorders/PLAYER_PVP_TALENT.lua
@@ -1,2 +1,33 @@
 TheEye.Recorder.Recorders.PLAYER_PVP_TALENT = {}
 local this = TheEye.Recorder.Recorders.PLAYER_PVP_TALENT
+
+local DataRecord = TheEye.Recorder.Managers.Recorders.DataRecord
+local EventsRegister = TheEye.Core.Managers.Events.Register
+local GetAllSelectedPvpTalentIDs = C_SpecializationInfo.GetAllSelectedPvpTalentIDs
+local table = table
+
+
+local function DataRecordIfNecessary()
+    local currentTalents = GetAllSelectedPvpTalentIDs()
+
+    if table.areidentical(currentTalents, previousTalents) == false then
+        local data = { talentIDs = currentTalents }
+        DataRecord(this, data)
+        previousTalents = currentTalents
+    end
+end
+
+function this.Initialize()
+    this.gameEvents = 
+    {
+        "PLAYER_PVP_TALENT_UPDATE",
+        "UNIT_INVENTORY_CHANGED",
+    }
+    EventsRegister(this)
+
+    DataRecordIfNecessary()
+end
+
+function this:OnEvent(event, ...)
+    DataRecordIfNecessary()
+end
