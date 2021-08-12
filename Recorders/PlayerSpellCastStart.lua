b94464eebf536b811553cf2b6dcddd1db2058502
EdvardXYZ <eddumansky@gmail.com>
2021-08-12 13:45:24 +0300

    Rename SpellCastStart to PlayerSpellCastStart

--- Recorders/PlayerSpellCastStart.lua
+++ Recorders/PlayerSpellCastStart.lua
@@ -1,5 +1,5 @@
-TheEye.Recorder.Recorders.SpellCastStart = {}
-local this = TheEye.Recorder.Recorders.SpellCastStart
+TheEye.Recorder.Recorders.PlayerSpellCastStart = {}
+local this = TheEye.Recorder.Recorders.PlayerSpellCastStart
 
 local DataRecord = TheEye.Recorder.Managers.Recorders.DataRecord
 local EventsRegister = TheEye.Core.Managers.Events.Register
@@ -19,6 +19,6 @@ function this:OnEvent(event, ...)
         {
             spellID = spellID,
         }
-        DataRecord("SpellCastStart", data)
+        DataRecord("PlayerSpellCastStart", data)
     end
 end
