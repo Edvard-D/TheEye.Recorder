7aeebacecba4cc33ae766e5b1e8575bed9498496
EdvardXYZ <eddumansky@gmail.com>
2021-08-12 20:26:47 +0300

    Implement PLAYER_IS_TANKING recorder

--- Recorders/PLAYER_IS_TANKING.lua
+++ Recorders/PLAYER_IS_TANKING.lua
@@ -1,2 +1,41 @@
 TheEye.Recorder.Recorders.PLAYER_IS_TANKING = {}
 local this = TheEye.Recorder.Recorders.PLAYER_IS_TANKING
+
+local DataRecord = TheEye.Recorder.Managers.Recorders.DataRecord
+local NotifyBasedFunctionCallerSetup = TheEye.Core.UI.Elements.ListenerGroups.NotifyBasedFunctionCaller.Setup
+
+
+function this.Initialize()
+    this.ListenerGroup =
+    {
+        Listeners =
+        {
+            {
+                eventEvaluatorKey = "UNIT_THREAT_SITUATION_CHANGED",
+                inputValues = { --[[unit]] "player", --[[otherUnit]] "_" },
+            },
+        },
+    }
+    NotifyBasedFunctionCallerSetup(
+        this.ListenerGroup,
+        this,
+        "Notify"
+    )
+    this.ListenerGroup:Activate()
+
+    local data =
+    {
+        isTanking = false,
+    }
+    DataRecord(this, data)
+end
+
+function this:Notify(event, ...)
+    local threatSituation = select(1, ...)
+    
+    local data =
+    {
+        isTanking = threatSituation >= 2,
+    }
+    DataRecord(this, data)
+end
