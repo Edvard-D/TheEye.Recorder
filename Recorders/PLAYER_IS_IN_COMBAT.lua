2ac17e232c4f199446132c450dbf3af5a606d3a9
EdvardXYZ <eddumansky@gmail.com>
2021-08-12 20:36:23 +0300

    Implement PLAYER_IS_IN_COMBAT recorder

--- Recorders/PLAYER_IS_IN_COMBAT.lua
+++ Recorders/PLAYER_IS_IN_COMBAT.lua
@@ -1,2 +1,30 @@
 TheEye.Recorder.Recorders.PLAYER_IS_IN_COMBAT = {}
 local this = TheEye.Recorder.Recorders.PLAYER_IS_IN_COMBAT
+
+local DataRecord = TheEye.Recorder.Managers.Recorders.DataRecord
+local EventsRegister = TheEye.Core.Managers.Events.Register
+local UnitAffectingCombat = UnitAffectingCombat
+
+
+function this.Initialize()
+    this.gameEvents =
+    {
+        "PLAYER_REGEN_DISABLED",
+        "PLAYER_REGEN_ENABLED",
+    }
+    EventsRegister(this)
+
+    local data =
+    {
+        isInCombat = UnitAffectingCombat("player"),
+    }
+    DataRecord(this, data)
+end
+
+function this:OnEvent(event, ...)
+    local data =
+    {
+        isInCombat = event == "PLAYER_REGEN_DISABLED",
+    }
+    DataRecord(this, data)
+end
