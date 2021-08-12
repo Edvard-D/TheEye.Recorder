04f68a60530bfc382b922b66c4279ab123ab91dc
EdvardXYZ <eddumansky@gmail.com>
2021-08-12 13:51:19 +0300

    Implement PlayerLevel recorder

--- Recorders/PlayerLevel.lua
+++ Recorders/PlayerLevel.lua
@@ -1,2 +1,22 @@
 TheEye.Recorder.Recorders.PlayerLevel = {}
 local this = TheEye.Recorder.Recorders.PlayerLevel
+
+local DataRecord = TheEye.Recorder.Managers.Recorders.DataRecord
+local EventsRegister = TheEye.Core.Managers.Events.Register
+local select = select
+
+
+function this.Initialize()
+    this.gameEvents = { "PLAYER_LEVEL_UP" }
+    EventsRegister(this)
+end
+
+function this:OnEvent(event, ...)
+    level = select(1, ...)
+
+    local data =
+    {
+        level = level,
+    }
+    DataRecord("PlayerLevel", data)
+end
