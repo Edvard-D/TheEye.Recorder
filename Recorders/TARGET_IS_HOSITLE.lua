6933501d703dc5c1872560daaf773021b976b463
EdvardXYZ <eddumansky@gmail.com>
2021-08-12 21:36:14 +0300

    Implement TARGET_IS_HOSTILE recorder

--- Recorders/TARGET_IS_HOSITLE.lua
+++ Recorders/TARGET_IS_HOSITLE.lua
@@ -1,2 +1,39 @@
 TheEye.Recorder.Recorders.TARGET_IS_HOSITLE = {}
 local this = TheEye.Recorder.Recorders.TARGET_IS_HOSITLE
+
+local DataRecord = TheEye.Recorder.Managers.Recorders.DataRecord
+local NotifyBasedFunctionCallerSetup = TheEye.Core.UI.Elements.ListenerGroups.NotifyBasedFunctionCaller.Setup
+
+
+function this.Initialize()
+    this.ListenerGroup =
+    {
+        Listeners =
+        {
+            {
+                eventEvaluatorKey = "UNIT_IS_HOSTILE_CHANGED",
+                inputValues = { --[[unit2]] "player", --[[unit2]] "target" },
+            },
+        },
+    }
+    NotifyBasedFunctionCallerSetup(
+        this.ListenerGroup,
+        this,
+        "Notify"
+    )
+    this.ListenerGroup:Activate()
+
+    local data =
+    {
+        isHostile = UnitIsEnemy("player", "target"),
+    }
+    DataRecord(this, data)
+end
+
+function this:Notify(event, isHostile)
+    local data =
+    {
+        isHostile = isHostile,
+    }
+    DataRecord(this, data)
+end
