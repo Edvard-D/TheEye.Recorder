70d8bc294f51f8e0993dde260714f590af3337fc
EdvardXYZ <eddumansky@gmail.com>
2021-08-12 20:07:56 +0300

    Implemenet PLAYER_HAS_TARGET recorder

--- Recorders/PLAYER_HAS_TARGET.lua
+++ Recorders/PLAYER_HAS_TARGET.lua
@@ -1,2 +1,28 @@
 TheEye.Recorder.Recorders.PLAYER_HAS_TARGET = {}
 local this = TheEye.Recorder.Recorders.PLAYER_HAS_TARGET
+
+local DataRecord = TheEye.Recorder.Managers.Recorders.DataRecord
+local EventsRegister = TheEye.Core.Managers.Events.Register
+local UnitGUID = UnitGUID
+
+
+function this.Initialize()
+    this.gameEvents = { "PLAYER_TARGET_CHANGED" }
+    EventsRegister(this)
+
+    local data =
+    {
+        hasTarget = false,
+    }
+    DataRecord(this, data)
+end
+
+function this:OnEvent(event, ...)
+    local hasTarget = UnitGUID("target") ~= nil
+
+    local data =
+    {
+        hasTarget = hasTarget,
+    }
+    DataRecord(this, data)
+end
