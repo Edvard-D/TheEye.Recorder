3410f3dac6d4250232eebaa04470f3e4d53fbb28
EdvardXYZ <eddumansky@gmail.com>
2021-08-12 16:53:10 +0300

    Implement PLAYER_EQUIPMENT recorder

--- Recorders/PLAYER_EQUIPMENT.lua
+++ Recorders/PLAYER_EQUIPMENT.lua
@@ -1,2 +1,41 @@
 TheEye.Recorder.Recorders.PLAYER_EQUIPMENT = {}
 local this = TheEye.Recorder.Recorders.PLAYER_EQUIPMENT
+
+local DataRecord = TheEye.Recorder.Managers.Recorders.DataRecord
+local EventsRegister = TheEye.Core.Managers.Events.Register
+local GetInventoryItemID = GetInventoryItemID
+local previousArmor = {}
+local table = table
+
+
+local function DataRecordIfNecessary()
+    local currentArmor = {}
+    
+    for i = 1, 19 do
+        local itemID = GetInventoryItemID("player", i)
+
+        if itemID ~= nil then
+            table.insert(currentArmor, itemID)
+        end
+    end
+
+    if table.areidentical(currentArmor, previousArmor) == false then
+        local data = { itemIDs = currentArmor }
+        DataRecord(this, data)
+        previousArmor = currentArmor
+    end
+end
+
+function this.Initialize()
+    this.gameEvents = 
+    {
+        "UNIT_INVENTORY_CHANGED",
+    }
+    EventsRegister(this)
+
+    DataRecordIfNecessary()
+end
+
+function this:OnEvent(event, ...)
+    DataRecordIfNecessary()
+end
