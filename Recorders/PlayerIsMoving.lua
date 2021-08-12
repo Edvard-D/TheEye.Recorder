b33ce304f84a26e4afb22e3145f29c1f40c75b1b
EdvardXYZ <eddumansky@gmail.com>
2021-08-12 14:25:13 +0300

    Implement PlayerIsMoving recorder

--- Recorders/PlayerIsMoving.lua
+++ Recorders/PlayerIsMoving.lua
@@ -1,2 +1,35 @@
 TheEye.Recorder.Recorders.PlayerIsMoving = {}
 local this = TheEye.Recorder.Recorders.PlayerIsMoving
+
+local DataRecord = TheEye.Recorder.Managers.Recorders.DataRecord
+local NotifyBasedFunctionCallerSetup = TheEye.Core.UI.Elements.ListenerGroups.NotifyBasedFunctionCaller.Setup
+
+
+function this.Initialize()
+    this.ListenerGroup =
+    {
+        Listeners =
+        {
+            {
+                eventEvaluatorKey = "UNIT_IS_MOVING_CHANGED",
+                inputValues = { --[[unit]] "player" },
+            },
+        },
+    }
+    NotifyBasedFunctionCallerSetup(
+        this.ListenerGroup,
+        this,
+        "Notify"
+    )
+    this.ListenerGroup:Activate()
+end
+
+function this:Notify(event, ...)
+    local isMoving = select(1, ...)
+    
+    local data =
+    {
+        isMoving = isMoving,
+    }
+    DataRecord("PlayerIsMoving", data)
+end
