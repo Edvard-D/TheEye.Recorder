bd44c531345c143a8f73afccc34d302730ba15d7
EdvardXYZ <eddumansky@gmail.com>
2021-08-13 12:55:32 +0300

    Implement UNIT_COUNT_CLOSE_TO_TARGET recorder

--- Recorders/UNIT_COUNT_CLOSE_TO_TARGET.lua
+++ Recorders/UNIT_COUNT_CLOSE_TO_TARGET.lua
@@ -1,2 +1,47 @@
 TheEye.Recorder.Recorders.UNIT_COUNT_CLOSE_TO_TARGET = {}
 local this = TheEye.Recorder.Recorders.UNIT_COUNT_CLOSE_TO_TARGET
+
+local DataRecord = TheEye.Recorder.Managers.Recorders.DataRecord
+local NotifyBasedFunctionCallerSetup = TheEye.Core.UI.Elements.ListenerGroups.NotifyBasedFunctionCaller.Setup
+
+
+function this.Initialize()
+    this.ListenerGroup =
+    {
+        Listeners =
+        {
+            {
+                eventEvaluatorKey = "UNIT_COUNT_CLOSE_TO_UNIT_CHANGED",
+                inputValues = { --[[unit]] "target", --[[hostilityMask]] COMBATLOG_OBJECT_REACTION_HOSTILE, },
+            },
+            {
+                eventEvaluatorKey = "UNIT_COUNT_CLOSE_TO_UNIT_CHANGED",
+                inputValues = { --[[unit]] "target", --[[hostilityMask]] COMBATLOG_OBJECT_REACTION_FRIENDLY, },
+            },
+        },
+    }
+    NotifyBasedFunctionCallerSetup(
+        this.ListenerGroup,
+        this,
+        "Notify"
+    )
+    this.ListenerGroup:Activate()
+end
+
+function this:Notify(event, unitCount, inputGroup)
+    local hostilityMask = inputGroup.inputValues[2]
+    local hostility
+
+    if hostilityMask == COMBATLOG_OBJECT_REACTION_HOSTILE then
+        hostility = "HOSTILE"
+    else
+        hostility = "FRIENDLY"
+    end
+
+    local data =
+    {
+        hostility = hostility,
+        unitCount = unitCount,
+    }
+    DataRecord(this, data)
+end
