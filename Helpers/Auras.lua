0c7a941def429f12a91c59db95b4dc6c7af37e99
EdvardXYZ <eddumansky@gmail.com>
2021-08-14 20:39:23 +0300

    Implement AreSourceAndDestUnitValidForRecord function

--- Helpers/Auras.lua
+++ Helpers/Auras.lua
@@ -1,2 +1,21 @@
 TheEye.Recorder.Helpers.Auras = {}
 local this = TheEye.Recorder.Helpers.Auras
+
+local UnitCategoryGet = TheEye.Core.Helpers.Unit.UnitCategoryGet
+local UnitIsEnemy = UnitIsEnemy
+local UnitIsPlayer = UnitIsPlayer
+
+
+function this.AreSourceAndDestUnitValidForRecord(sourceUnit, sourceUnitCategory, destUnit)
+    local destUnitIsFriendlyPlayer = false
+    if destUnit ~= nil then
+        destUnitIsFriendlyPlayer = UnitIsEnemy(destUnit) == false and UnitIsPlayer(destUnit) == true
+    end
+    
+    return destUnit == "player"
+        or (destUnitIsFriendlyPlayer == true
+            and (sourceUnitCategory == "NONE"
+                or sourceUnitCategory == "BOSS"
+                or sourceUnitCategory == "OTHER"))
+        or sourceUnit == "player"
+end
