ca6450a647259e5ec9b89030953f40a6358e8a76
EdvardXYZ <eddumansky@gmail.com>
2021-08-14 14:49:47 +0300

    Implement PLAYER_SPELL_CAST_SUCCEEDED recorder

--- Recorders/PLAYER_SPELL_CAST_SUCCEEDED.lua
+++ Recorders/PLAYER_SPELL_CAST_SUCCEEDED.lua
@@ -1,2 +1,19 @@
 TheEye.Recorder.Recorders.PLAYER_SPELL_CAST_SUCCEEDED = {}
 local this = TheEye.Recorder.Recorders.PLAYER_SPELL_CAST_SUCCEEDED
+
+local DataRecord = TheEye.Recorder.Managers.Recorders.DataRecord
+local EventsRegister = TheEye.Core.Managers.Events.Register
+
+
+function this.Initialize()
+    this.gameEvents = { "UNIT_SPELLCAST_SUCCEEDED" }
+    EventsRegister(this)
+end
+
+function this:OnEvent(event, ...)
+    unit, _, spellID = ...
+
+    if unit == "player" then
+        DataRecord(this, spellID)
+    end
+end
