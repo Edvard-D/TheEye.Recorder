d77d577e02b655de5f2f9b51db643b0290a88494
EdvardXYZ <eddumansky@gmail.com>
2021-08-14 17:08:58 +0300

    Implement PLAYER_INSTANCE recorder

--- Recorders/PLAYER_INSTANCE.lua
+++ Recorders/PLAYER_INSTANCE.lua
@@ -1,2 +1,21 @@
 TheEye.Recorder.Recorders.PLAYER_INSTANCE = {}
 local this = TheEye.Recorder.Recorders.PLAYER_INSTANCE
+
+local DataRecord = TheEye.Recorder.Managers.Recorders.DataRecord
+local EventsRegister = TheEye.Core.Managers.Events.Register
+local GetInstanceInfo = GetInstanceInfo
+local select = select
+
+
+function this.Initialize()
+    this.gameEvents = { "PLAYER_ENTERING_WORLD" }
+    EventsRegister(this)
+
+    local instanceID = select(8, GetInstanceInfo)
+    DataRecord(this, instanceID)
+end
+
+function this:OnEvent(event, ...)
+    local instanceID = select(8, GetInstanceInfo)
+    DataRecord(this, instanceID)
+end
