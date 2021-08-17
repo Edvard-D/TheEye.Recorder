fed7338d4d64acdf3b731dcd22138bb1307c4a91
EdvardXYZ <eddumansky@gmail.com>
2021-08-17 10:58:44 +0300

    Rename TARGET_SPELL_CAST_DURATION

---
+++ Recorders/TARGET_SPELLCAST_IS_ACTIVE.lua
@@ -0,0 +1,2 @@
+TheEye.Recorder.Recorders.TARGET_SPELLCAST_IS_ACTIVE = {}
+local this = TheEye.Recorder.Recorders.TARGET_SPELLCAST_IS_ACTIVE
--- Recorders/TARGET_SPELL_CAST_DURATION.lua
+++
@@ -1,2 +0,0 @@
-TheEye.Recorder.Recorders.TARGET_SPELL_CAST_DURATION = {}
-local this = TheEye.Recorder.Recorders.TARGET_SPELL_CAST_DURATION
