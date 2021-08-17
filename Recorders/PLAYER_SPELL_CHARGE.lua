24b0433a93177b2019242c11c4cf1add06f71360
EdvardXYZ <eddumansky@gmail.com>
2021-08-17 12:34:12 +0300

    Implement PLAYER_SPELL_CHARGE recorder

--- Recorders/PLAYER_SPELL_CHARGE.lua
+++ Recorders/PLAYER_SPELL_CHARGE.lua
@@ -1,2 +1,35 @@
 TheEye.Recorder.Recorders.PLAYER_SPELL_CHARGE = {}
 local this = TheEye.Recorder.Recorders.PLAYER_SPELL_CHARGE
+
+local CurrentSpellsGet = TheEye.Core.Helpers.Spells.CurrentSpellsGet
+local DataRecord = TheEye.Recorder.Managers.Recorders.DataRecord
+local EventsRegister = TheEye.Core.Managers.Events.Register
+local GetSpellCharges = GetSpellCharges
+local previousValues = {}
+local select = select
+
+
+local function DataRecordIfNecessary()
+    local currentSpellIDs = CurrentSpellsGet(true)
+
+    for i = 1, #currentSpellIDs do
+        local spellID = currentSpellIDs[i]
+        local charges = select(1, GetSpellCharges(spellID))
+
+        if charges ~= nil and previousValues[spellID] ~= charges then
+            DataRecord(this, spellID .. "_" .. charges)
+            previousValues[spellID] = charges
+        end
+    end
+end
+
+function this.Initialize()
+    this.gameEvents = { "SPELL_UPDATE_CHARGES" }
+    EventsRegister(this)
+    
+    DataRecordIfNecessary()
+end
+
+function this:OnEvent(event, unit, _, spellID)
+    DataRecordIfNecessary()
+end
